c8e03dc995e50fe118457bf4e4d72fa916b19ea1
1900369667 <1900369667@qq.com>
2025-04-11 13:00:42 +0800

    定长滑动窗口练习

---
+++ src/slidingwindow/findMaxAverage_643.java
@@ -0,0 +1,23 @@
+package slidingwindow;
+
+public class findMaxAverage_643 {
+    /**
+     * 找到定长k的窗口中最大的平均数
+     * @param nums 给定数组
+     * @param k 定长窗口长度k
+     * @return 最大的平均数
+     */
+    public double findMaxAverage(int[] nums, int k) {
+        int ant = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(i<k-1){
+                sum += nums[i];
+                continue;
+            }
+            ant = Math.max(sum,ant);
+            sum -= nums[i-k+1];
+        }
+        return (double) ant/k;
+    }
+}
