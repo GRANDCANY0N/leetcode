f1604aaf058329e36b5c0cb1b580654c9a83c526
1900369667 <1900369667@qq.com>
2025-04-21 16:14:54 +0800

    定长滑动窗口练习

---
+++ src/slidingwindow/MinimumDifference1984.java
@@ -0,0 +1,31 @@
+package slidingwindow;
+
+import java.util.Arrays;
+
+/**
+ * @author Canyon
+ */
+public class MinimumDifference1984 {
+    /**
+     * 先排序，就变成了模板题
+     * @param nums 原数组
+     * @param k 滑动窗口的长度，但是可以不连续
+     * @return 滑动窗口中最小的最大最小差值
+     */
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min = 9999999;
+        for (int i = 0; i < nums.length; i++) {
+            if(i<k-1){
+                continue;
+            }
+            min = Math.min(min, nums[i] - nums[i-k+1]);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9,4,1,7};
+        System.out.println(new MinimumDifference1984().minimumDifference(arr,2));
+    }
+}
