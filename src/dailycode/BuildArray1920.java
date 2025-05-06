41f2c401a34e2b8dd084edf1b2c3ecd5e4ad884c
1900369667 <1900369667@qq.com>
2025-05-06 11:20:23 +0800

    每日一题

--- src/dailycode/BuildArray1920.java
+++ src/dailycode/BuildArray1920.java
@@ -4,6 +4,11 @@ package dailycode;
  * @author Canyon
  */
 public class BuildArray1920 {
+    /**
+     * 符合要求遍历
+     * @param nums 原数组
+     * @return {@link int[] } 按要求排序后的数组
+     */
     public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
