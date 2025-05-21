f2dd14818e99cfb8d1f88b3feef696fcf13e53ae
1900369667 <1900369667@qq.com>
2025-05-21 16:23:41 +0800

    最短最小滑动窗口

--- src/slidingwindowsanddoublepointers/varsliwin/shortestorminimum/MinSubArrayLen209.java
+++ src/slidingwindowsanddoublepointers/varsliwin/shortestorminimum/MinSubArrayLen209.java
@@ -4,6 +4,12 @@ package slidingwindowsanddoublepointers.varsliwin.shortestorminimum;
  * @author Canyon
  */
 public class MinSubArrayLen209 {
+    /**
+     * left维护左边，当满足和大于等于target时移动left，并计算子串长度
+     * @param target 目标值
+     * @param nums 条件数组
+     * @return int 最短的子串大于等于target的长度
+     */
     public int minSubArrayLen(int target, int[] nums) {
         int sum=0;int left=0;
         int ans=Integer.MAX_VALUE;boolean f=false;
