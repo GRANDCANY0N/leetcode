06a9cf8e990a95b52858badeca7d3e08a586d43a
canyon <1900369667@qq.com>
2025-06-14 10:30:52 +0800

    将二分查找方法封装

---
+++ src/binarysearch/search/helper/BinarySearchHelper.java
@@ -0,0 +1,17 @@
+package binarysearch.search.helper;
+
+public class BinarySearchHelper {
+    public int lower_bound(int[] nums, int target) {
+        int left = -1;
+        int right = nums.length;
+        while (left+1 < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target){
+                left = mid;
+            }else{
+                right = mid;
+            }
+        }
+        return right;
+    }
+}
