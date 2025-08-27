1b804e2338e002053ecbd53aca89807134ee0257
1900369667 <1900369667@qq.com>
2025-08-27 11:34:09 +0800

    基本数据结构简单题 补充注释

--- src/datastructure/enumtech/enumrmaintainl/base/MaximumDifference2016.java
+++ src/datastructure/enumtech/enumrmaintainl/base/MaximumDifference2016.java
@@ -7,12 +7,16 @@ package datastructure.enumtech.enumrmaintainl.base;
 public class MaximumDifference2016 {
     /**
      * 整体思路：
+     * 由于必须是小值在前，大值在后
+     * 循环时不断用当前值减去现存的小值，比较是否是最大差值的同时更新当前值和小值之间的最小值
      * -
      * 步骤：
+     * 初始化最小值为最大数，ans等于-1
+     * 遍历数组，更新ans和当前值减去minNum的最大值，并将minNum更新为与当前值中的较小者
      * -
      * 复杂度：
-     * - 时间复杂度： O()
-     * - 空间复杂度： O()
+     * - 时间复杂度： O(N) 一次遍历
+     * - 空间复杂度： O(1) 常数级变量
      * -
      * @param nums 整数数组
      * @return int 返回 nums[j] - nums[i] 能求得的 最大差值
--- src/datastructure/enumtech/enumrmaintainl/base/MinimumCardPickup2260.java
+++ src/datastructure/enumtech/enumrmaintainl/base/MinimumCardPickup2260.java
@@ -9,15 +9,19 @@ import java.util.HashMap;
 public class MinimumCardPickup2260 {
     /**
      * 整体思路：
+     * HashMap记录每个值的出现次数，滑动窗口找到两个值相等的最小窗口
      * -
      * 步骤：
+     * 用hashMap记录每个值出现的次数
+     * 遍历数组，先将这个值的索引和出现次数作为key和value存入hashMap
+     * 若出现次数等于两次，收缩左边的窗口直至当前值的出现次数小于两次，并更新此时窗口大小是否最小
      * -
      * 复杂度：
-     * - 时间复杂度： O()
-     * - 空间复杂度： O()
+     * - 时间复杂度： O(N) 滑动窗口一次遍历
+     * - 空间复杂度： O(N) hashMap
      * -
-     * @param cards
-     * @return int
+     * @param cards 整数数组
+     * @return int 返回值相等的最小窗口长度
      */
     public int minimumCardPickup(int[] cards) {
         int ans = Integer.MAX_VALUE;
@@ -37,15 +41,19 @@ public class MinimumCardPickup2260 {
 
     /**
      * 整体思路：
+     * hashMap存储值以及索引，每次计算索引差值之后更新相同值的索引
      * -
      * 步骤：
+     * hashMap存储值以及索引
+     * 遍历数组，若hashMap中未存在这个值及其索引，将值和索引存入并进行下一次循环
+     * 若已存在索引，说明出现了两次相同的值，计算索引差值+1与ans比较得出最小的差值并更新索引为最新的
      * -
      * 复杂度：
-     * - 时间复杂度： O()
-     * - 空间复杂度： O()
+     * - 时间复杂度： O(N) 滑动窗口一次遍历
+     * - 空间复杂度： O(N) hashMap
      * -
-     * @param cards
-     * @return int
+     * @param cards 整数数组
+     * @return int 返回值相等的最小窗口长度
      */
     public int minimumCardPickupTwo(int[] cards) {
         int ans = Integer.MAX_VALUE;
